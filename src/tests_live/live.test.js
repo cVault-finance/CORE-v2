1c675f43651eab75482b291ea360e20cf205c318
0xRevert <coinphd@protonmail.com>
2020-10-24 13:54:57 -0700

    remove upgrade in testst since its mainnet

--- src/tests_live/live.test.js
+++ src/tests_live/live.test.js
@@ -87,12 +87,12 @@ contract('LGE Live Tests', ([x3, pervert, rando, joe, john, trashcan]) => {
     beforeEach(async () => {
         this.owner = "0x5A16552f59ea34E44ec81E58b3817833E9fD5436";
         this.OxRevertMainnetAddress = '0xd5b47B80668840e7164C1D1d81aF8a9d9727B421';
-        this.CORETransferHandler = await TransferHandler01.new({ from: pervert });
+        // this.CORETransferHandler = await TransferHandler01.new({ from: pervert });
         //This is now upgraded
-        let globalsLive = await COREGlobals.at(CORE_GLOBALS_ADDRESS);
+        // let globalsLive = await COREGlobals.at(CORE_GLOBALS_ADDRESS);
         // we setnew transfer handler to transfer handler
 
-        await globalsLive.setTransferHandler(this.CORETransferHandler.address, { from: this.owner });
+        // await globalsLive.setTransferHandler(this.CORETransferHandler.address, { from: this.owner });
         this.LGEUpgrade = await LGE.new({ from: pervert, gasLimit: 50000000 });
         this.iLGE = await LGE.at(LGE_2_PROXY_ADDRESS);
         let proxyAdmin = await ProxyAdminContract.at(proxyAdmin_ADDRESS);
